31b1cba83a3482cac6c7e67ab615ff0ed8f82ebc
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2023-10-19 21:02:35 -0600

    Encapsulamiento

--- 3.1.2_IntroAPOO/script.js
+++ 3.1.2_IntroAPOO/script.js
@@ -16,17 +16,40 @@ Persona
 
 class Persona{
 
+    #edad;
+    #mejorAmigo;
+
     constructor(nombre, edad, genero, nacionalidad){
         this.nombre = nombre;
-        this.edad = edad;
+        this.#edad = edad;
         this.genero = genero;
         this.nacionalidad = nacionalidad;
         this.horasSuenio = 0;
-        this.mejorAmigo = null;
+        this.#mejorAmigo = null;
+    }
+
+    getEdad(){
+        return this.#edad;
+    }
+
+    setEdad(edad){
+        if(edad < 0){
+            this.#edad = 0;
+        }else{
+            this.#edad = edad;
+        }
+    }
+
+    getMejorAmigo(){
+        return this.#mejorAmigo;
+    }
+
+    setMejorAmigo(mejorAmigo){
+        this.#mejorAmigo = mejorAmigo;
     }
 
     saludar(){
-        console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años`);
+        console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.#edad} años`);
     }
 
     dormir(horasDormidas){
@@ -51,3 +74,26 @@ console.log(personaA.horasSuenio);
 const personaB = new Persona('Juan', 23, 'Masculino', 'Colombiana');
 
 personaA.platicar(personaB);
+
+console.log(`La edad de ${personaA.nombre} es ${personaA.getEdad()}`);
+personaA.setEdad(25);
+console.log(`La edad de ${personaA.nombre} es ${personaA.getEdad()}`);
+
+personaA.setMejorAmigo(personaB);
+
+console.log(`El mejor amigo de ${personaA.nombre} es ${personaA.getMejorAmigo().nombre}`);
+
+
+//Ejercicio:
+/*
+Crear una clase llamada  Contacto con los siguientes datos*
+Nombre 
+Apellidos
+Telefono
+
+
+Después crear una clase que sea ListaContactos la cual tendrá dos métodos
+Agregar un contacto
+Buscar un contacto por nombre
+*/
+
